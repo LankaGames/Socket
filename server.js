21c9f949b993d09f16d4ef2eb0eb187a6f92f7de
Darsan <lankagames1991@gmail.com>
2016-09-03 13:33:11 +0530

    Setup Connection in IO

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ var PORT = process.env.port || 3000;
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
+var socketio = require("socket.io")(http);
+
 
 app.use(express.static(__dirname+"/public"));
+
+socketio.on("connection",function(){
+    console.log("connection event");
+});
 http.listen(PORT,function(){
             console.log("Server Started");
             });
